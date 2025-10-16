1591cb08827c68c0a08b4f519ea26f4cfaa0319c
peridoll <S.yoshito.2088@gmail.com>
2025-10-16 22:02:49 +0900

    0.10.3 - 4

--- CRONICA/resource_zip.bat
+++ CRONICA/resource_zip.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set TARGET_FOLDER=resource\
+set TARGET_FOLDER=resource\*
 set ZIP_NAME=resource.zip
 
 powershell -Command Compress-Archive -Path '%TARGET_FOLDER%' -DestinationPath '%ZIP_NAME%' -Force
